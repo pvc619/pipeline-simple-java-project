fe4442d2ac1b142a27094a14fb52960d331443ab
pvc619 <112956648+pvc619@users.noreply.github.com>
2024-05-29 19:37:22 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,9 +5,6 @@ public class App {
 
 <<<<<<< HEAD
         System.out.println("Hello, Jenkins 10!!!");
-=======
-        System.out.println("Hello, Jenkins 10!!!");
->>>>>>> 4f7f73eee7c496521b4a7e7cba1ceff6010edad2
     }
 }
 
