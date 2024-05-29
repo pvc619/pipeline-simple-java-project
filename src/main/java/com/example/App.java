4f7f73eee7c496521b4a7e7cba1ceff6010edad2
pvc619 <112956648+pvc619@users.noreply.github.com>
2024-05-29 17:33:25 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,7 +3,7 @@ package com.example;
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("Hello, Jenkins 10!!!")
+        System.out.println("Hello, Jenkins 10!!!");
     }
 }
 
